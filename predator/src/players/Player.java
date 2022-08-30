b38a95a0f2d08b3c8067989e93d90e01754cb61d
DishaRevandkar <55402567+DishaRevandkar@users.noreply.github.com>
2022-08-29 22:19:50 -0400

    Add files via upload

---
+++ predator/src/players/Player.java
@@ -0,0 +1,27 @@
+package players;
+
+/**
+ * An interface to represent a player in the game. A player is any entity
+ * that can come into conflict with another player, where battle yields
+ * one victor and one loser.
+ *
+ * @author Disha Revandkar
+ * @author Shreya Pramod
+ */
+public interface Player {
+
+    /**
+     * when this player defeats another player.
+     *
+     * @param player the player I defeated
+     */
+    void victory (Player player);
+
+    /**
+     * when this player loses to another player.
+     *
+     * @param player the player I defeated
+     */
+    void defeat (Player player);
+
+}
